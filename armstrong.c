796eee3fba7509de2334dcd9d4f46c905d5a3569
Mandeep Singh <146833671+Mandeepbala7@users.noreply.github.com>
2023-11-28 17:07:16 +0530

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int number,originalnumber,remainder,result=0,n=0;
+    printf("enter the integer:");
+    scanf("%d",&number);
+    originalnumber=number;
+    while(originalnumber!=0)
+    {
+        originalnumber/=10;
+        ++n;
+    }
+    originalnumber=number;
+    while(originalnumber!=0)
+    {
+        remainder=originalnumber%10;
+        result+=pow(remainder,n);
+        originalnumber/=10;
+    }
+    if(result==number)
+    {
+        printf("%dis a armstrong",number);
+    }
+    else
+    {
+        printf("%d is not anarmstrong ",number);
+    }
+}
